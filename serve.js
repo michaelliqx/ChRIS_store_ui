f038d231968025a34a530a68fd01244dad813e63
Qingxing Li <lqx1996@bu.edu>
2019-04-09 13:23:47 -0400

    modify ip and port

--- serve.js
+++ serve.js
@@ -3,10 +3,8 @@ const express = require('express');
 const { join } = require('path');
 
 // constants
-// const PORT = process.env.PORT || 3000;
-// const HOST = process.env.HOST || '0.0.0.0';
-const PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-const IP = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+const PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const IP = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 // serve static files
 const app = express();
 app.use(express.static('build'));
